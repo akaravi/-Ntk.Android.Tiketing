8c72e878f6bf039793f06b9e9dcdb4957432e2a8
Karavi Office <karavi.alireza@gmail.com>
2019-05-28 11:33:36 +0430

    بازگزاری جدید

---
+++ app/src/main/java/ntk/android/ticketing/room/RoomDb.java
@@ -0,0 +1,30 @@
+package ntk.android.ticketing.room;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import ntk.android.ticketing.BuildConfig;
+import ntk.android.ticketing.model.Notify;
+
+
+@Database(entities = {Notify.class}, version = 1, exportSchema = false)
+public abstract class RoomDb extends RoomDatabase {
+
+    public abstract NotificationDoa NotificationDoa();
+
+    private static RoomDb Instance;
+
+    public static RoomDb getRoomDb(Context context) {
+        if (Instance == null) {
+            Instance = Room.databaseBuilder(context.getApplicationContext(), RoomDb.class, BuildConfig.APPLICATION_ID)
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return Instance;
+    }
+
+    public static void DestroyInstance() {
+        Instance = null;
+    }
+}
