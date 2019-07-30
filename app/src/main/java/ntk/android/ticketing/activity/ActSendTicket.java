fc1582c8edf3442be5b920beb8162d6427448c78
me <zsdg98@gmail.com>
2019-07-30 13:45:12 +0430

    Fix Bugs.

--- app/src/main/java/ntk/android/ticketing/activity/ActSendTicket.java
+++ app/src/main/java/ntk/android/ticketing/activity/ActSendTicket.java
@@ -3,6 +3,7 @@ package ntk.android.ticketing.activity;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,9 +11,11 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
@@ -24,14 +27,11 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.codekidlabs.storagechooser.StorageChooser;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
-import com.tedpark.tedpermission.rx2.TedRx2Permission;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -108,14 +108,11 @@ public class ActSendTicket extends AppCompatActivity {
     @BindView(R.id.mainLayoutActSendTicket)
     CoordinatorLayout layout;
 
-    @BindView(R.id.progressAttachActSendTicket)
-    ProgressBar progressBar;
-
     private TicketingSubmitRequest request = new TicketingSubmitRequest();
     private List<String> attaches = new ArrayList<>();
     private AdAttach adapter;
     private String linkFileIds = "";
-    private static final int READ_REQUEST_CODE = 42;
+    private static final int READ_REQUEST_CODE = 1520;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -313,51 +310,7 @@ public class ActSendTicket extends AppCompatActivity {
         }
     }
 
-    public static String getFilePathFromUri(Context context, Uri _uri) {
-        String filePath = "";
-        if (_uri != null && "content".equals(_uri.getScheme())) {
-            try (Cursor cursor = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null)) {
-                if (cursor == null) return _uri.getPath();
-                cursor.moveToFirst();
-                filePath = cursor.getString(0);
-            } catch (SecurityException e) {
-                filePath = "/storage/emulated/0" + _uri.getPath().replace("/storage/emulated/0", "");
-            }
-
-        } else {
-            filePath = _uri.getPath();
-        }
-        return filePath;
-    }
-
-    public static String getFilePathFromUriForServer(Context context, Uri _uri) {
-        String filePath = "";
-        if (_uri != null && "content".equals(_uri.getScheme())) {
-            try (Cursor cursor = context.getContentResolver().query(_uri,
-                    new String[]
-                            {
-                                    MediaStore.Images.ImageColumns.DATA,
-                                    MediaStore.Images.Media.DATA,
-                                    MediaStore.Images.Media.MIME_TYPE,
-                                    MediaStore.Video.VideoColumns.DATA,
-                                    MediaStore.Video.Media.DATA,
-                                    MediaStore.Video.Media.MIME_TYPE,
-                                    MediaStore.Audio.AudioColumns.DATA,
-                                    MediaStore.Audio.Media.DATA,
-                                    MediaStore.Audio.Media.MIME_TYPE,
-                            }, null, null, null)) {
-                cursor.moveToFirst();
-                filePath = cursor.getString(0);
-            } catch (SecurityException e) {
-                filePath = "/storage/emulated/0" + _uri.getPath().replace("/storage/emulated/0", "");
-            }
-
-        } else {
-            filePath = _uri.getPath();
-        }
-        return filePath;
-    }
-
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
@@ -367,12 +320,90 @@ public class ActSendTicket extends AppCompatActivity {
                 uri = resultData.getData();
                 if (uri != null) {
                     Btn.setVisibility(View.GONE);
-                    attaches.add(getFilePathFromUri(ActSendTicket.this, uri));
+                    attaches.add(getPath(ActSendTicket.this, uri));
                     adapter.notifyDataSetChanged();
-                    UploadFileToServer(getFilePathFromUriForServer(ActSendTicket.this, uri));
+                    UploadFileToServer(getPath(ActSendTicket.this, uri));
                 }
             }
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String getPath(final Context context, final Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 
     private boolean CheckPermission() {
@@ -408,7 +439,6 @@ public class ActSendTicket extends AppCompatActivity {
 
                         @Override
                         public void onError(Throwable e) {
-                            progressBar.setVisibility(View.GONE);
                             Btn.setVisibility(View.VISIBLE);
                             Snackbar.make(layout, "خطای سامانه مجددا تلاش کنید", Snackbar.LENGTH_INDEFINITE).setAction("تلاش مجددا", new View.OnClickListener() {
                                 @Override
@@ -425,7 +455,6 @@ public class ActSendTicket extends AppCompatActivity {
                     });
         } else {
             Btn.setVisibility(View.VISIBLE);
-            progressBar.setVisibility(View.GONE);
             Toasty.warning(this, "عدم دسترسی به اینترنت", Toasty.LENGTH_LONG, true).show();
         }
     }
--- app/src/main/java/ntk/android/ticketing/activity/ActTicketAnswer.java
+++ app/src/main/java/ntk/android/ticketing/activity/ActTicketAnswer.java
@@ -2,6 +2,7 @@ package ntk.android.ticketing.activity;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,9 +10,12 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -21,7 +25,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -249,42 +252,7 @@ public class ActTicketAnswer extends AppCompatActivity {
         }
     }
 
-    public static String getFilePathFromUri(Context context, Uri _uri) {
-        String filePath = "";
-        if (_uri != null && "content".equals(_uri.getScheme())) {
-            try (Cursor cursor = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null)) {
-                if (cursor == null) return _uri.getPath();
-                cursor.moveToFirst();
-                filePath = cursor.getString(0);
-            } catch (SecurityException e) {
-                filePath = "/storage/emulated/0" + _uri.getPath().replace("/storage/emulated/0", "");
-            }
-
-        } else {
-            filePath = _uri.getPath();
-        }
-        return filePath;
-    }
-
-    public static String getFilePathFromUriForServer(Context context, Uri _uri) {
-        String filePath = "";
-        if (_uri != null && "content".equals(_uri.getScheme())) {
-            try (Cursor cursor = context.getContentResolver().query(_uri,
-                    new String[]{MediaStore.Images.Media.DATA},
-                    null, null, null)) {
-                if (cursor == null) return _uri.getPath();
-                cursor.moveToFirst();
-                filePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
-            } catch (SecurityException e) {
-                filePath = "/storage/emulated/0" + _uri.getPath().replace("/storage/emulated/0", "");
-            }
-
-        } else {
-            filePath = _uri.getPath();
-        }
-        return filePath;
-    }
-
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
@@ -294,12 +262,90 @@ public class ActTicketAnswer extends AppCompatActivity {
                 uri = resultData.getData();
                 if (uri != null) {
                     btn.setVisibility(View.GONE);
-                    attaches.add(getFilePathFromUri(ActTicketAnswer.this, uri));
+                    attaches.add(getPath(ActTicketAnswer.this, uri));
                     AdAtach.notifyDataSetChanged();
-                    UploadFileToServer(getFilePathFromUriForServer(ActTicketAnswer.this, uri));
+                    UploadFileToServer(getPath(ActTicketAnswer.this, uri));
                 }
             }
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String getPath(final Context context, final Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 
     private boolean CheckPermission() {
