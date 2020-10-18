3d2861b8bb4cc11ec242ad3c368687d136ca29fd
ixiflutter1313 <mj.parishani@outlook.com>
2020-10-18 22:32:55 +0330

    Update ActFaq.java

--- app/src/main/java/ntk/android/ticketing/activity/ActFaq.java
+++ app/src/main/java/ntk/android/ticketing/activity/ActFaq.java
@@ -2,18 +2,17 @@ package ntk.android.ticketing.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import android.widget.TextView;
 
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import es.dmoral.toasty.Toasty;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -23,6 +22,7 @@ import ntk.android.ticketing.R;
 import ntk.android.ticketing.adapter.AdFaq;
 import ntk.android.ticketing.config.ConfigRestHeader;
 import ntk.android.ticketing.config.ConfigStaticValue;
+import ntk.android.ticketing.utill.AppUtill;
 import ntk.android.ticketing.utill.FontManager;
 import ntk.base.api.ticket.interfase.ITicket;
 import ntk.base.api.ticket.model.TicketingFaqRequest;
@@ -48,44 +48,54 @@ public class ActFaq extends BaseActivity {
     private void init() {
         Lbl.setTypeface(FontManager.GetTypeface(this, FontManager.IranSans));
         Lbl.setText("پرسش های متداول");
-
         Rv.setHasFixedSize(true);
         Rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        //todo show loading
-        RetrofitManager retro = new RetrofitManager(this);
-        ITicket iTicket = retro.getCachedRetrofit(new ConfigStaticValue(this).GetApiBaseUrl()).create(ITicket.class);
-        Map<String, String> headers = new ConfigRestHeader().GetHeaders(this);
+        if (AppUtill.isNetworkAvailable(this)) {
+            // show loading
+            switcher.showProgressView();
+            RetrofitManager retro = new RetrofitManager(this);
+            ITicket iTicket = retro.getCachedRetrofit(new ConfigStaticValue(this).GetApiBaseUrl()).create(ITicket.class);
+            Map<String, String> headers = new ConfigRestHeader().GetHeaders(this);
 
-        TicketingFaqRequest request = new TicketingFaqRequest();
-        request.RowPerPage = 100;
+            TicketingFaqRequest request = new TicketingFaqRequest();
+            request.RowPerPage = 1000;
 
-        Observable<TicketingFaqResponse> Call = iTicket.GetTicketFaqActList(headers, request);
-        Call.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<TicketingFaqResponse>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
+            Observable<TicketingFaqResponse> Call = iTicket.GetTicketFaqActList(headers, request);
+            Call.subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<TicketingFaqResponse>() {
+                        @Override
+                        public void onSubscribe(Disposable d) {
 
-                    }
+                        }
 
-                    @Override
-                    public void onNext(TicketingFaqResponse model) {
-                        AdFaq adapter = new AdFaq(ActFaq.this, model.ListItems);
-                        Rv.setAdapter(adapter);
-                        adapter.notifyDataSetChanged();
-                    }
+                        @Override
+                        public void onNext(TicketingFaqResponse model) {
+                            AdFaq adapter = new AdFaq(ActFaq.this, model.ListItems);
+                            Rv.setAdapter(adapter);
+                            adapter.notifyDataSetChanged();
+                            if (adapter.getItemCount() > 0)
+                                switcher.showContentView();
+                            else
+                                switcher.showEmptyView();
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Toasty.warning(ActFaq.this, "خطای سامانه", Toasty.LENGTH_LONG, true).show();
+                        }
 
-                    }
+                        @Override
+                        public void onError(Throwable e) {
+                            switcher.showErrorView("خطای سامانه مجددا تلاش کنید", () -> init());
 
-                    @Override
-                    public void onComplete() {
+                        }
 
-                    }
-                });
+                        @Override
+                        public void onComplete() {
+
+                        }
+                    });
+        } else {
+            switcher.showErrorView("عدم دسترسی به اینترنت", () -> init());
+
+        }
     }
 
     @OnClick(R.id.imgBackActFaq)
