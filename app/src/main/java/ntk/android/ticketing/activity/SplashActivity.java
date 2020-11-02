8eded09e95d72c11af07585a04b81369ee6c292c
ixiflutter1313 <mj.parishani@outlook.com>
2020-11-02 13:48:21 +0330

    Update SplashActivity.java

--- app/src/main/java/ntk/android/ticketing/activity/SplashActivity.java
+++ app/src/main/java/ntk/android/ticketing/activity/SplashActivity.java
@@ -7,13 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Map;
 
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import ntk.android.base.config.ConfigRestHeader;
 import ntk.android.base.api.news.entity.NewsContent;
+import ntk.android.base.config.ConfigRestHeader;
+import ntk.android.base.config.NtkObserver;
 import ntk.android.base.entityModel.base.ErrorException;
 import ntk.android.base.entityModel.base.FilterModel;
 import ntk.android.base.services.news.NewsContentService;
@@ -28,28 +27,17 @@ public class SplashActivity extends AppCompatActivity {
         request.CurrentPageNumber = 1;
         Map<String, String> headers = new ConfigRestHeader().GetHeaders(this);
         new NewsContentService(this).getAll(request).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<ErrorException<NewsContent>>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                String w = "asdwa";
-            }
-
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new NtkObserver<ErrorException<NewsContent>>() {
             @Override
             public void onNext(@NonNull ErrorException<NewsContent> newsContentErrorException) {
-
                 String w = "asdwa";
 //                newsContentErrorException.Item.Body;
             }
-
             @Override
             public void onError(@NonNull Throwable e) {
                 String w = "asdwa";
             }
 
-            @Override
-            public void onComplete() {
-                String w = "asdwa";
-            }
         });
 
     }
