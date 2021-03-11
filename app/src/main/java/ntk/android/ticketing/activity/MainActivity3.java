a6463c9fe0e2b51d6975fadcb16bef37b7526cc6
ixiflutter1313 <mj.parishani@outlook.com>
2021-03-11 11:17:04 +0330

    add theme3

--- app/src/main/java/ntk/android/ticketing/activity/MainActivity3.java
+++ app/src/main/java/ntk/android/ticketing/activity/MainActivity3.java
@@ -1,7 +1,36 @@
 package ntk.android.ticketing.activity;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import ntk.android.base.activity.abstraction.AbstractMainActivity;
+import ntk.android.base.dtomodel.theme.DrawerChildThemeDtoModel;
+import ntk.android.ticketing.R;
 
-class MainActivity3 extends AbstractMainActivity {
+public class MainActivity3 extends AbstractMainActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_3_acitivty);
+        List<DrawerChildThemeDtoModel> menus = createDrawerItems();
+    }
 
+    private List<DrawerChildThemeDtoModel> createDrawerItems() {
+        ArrayList<DrawerChildThemeDtoModel> list = new ArrayList<>();
+        int i = 0;
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("صندوق پیام").setIcon(R.drawable.notification2));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("اخبار"));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("نظرسنجی"));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("دعوت از دوستان"));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("درباره ما"));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("تماس با ما"));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("بازخورد"));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("پرسش های متداول"));
+        list.add(new DrawerChildThemeDtoModel().setId(i++).setTitle("وبلاگ"));
+
+    }
 }
---
+++ app/src/main/java/ntk/android/ticketing/adapter/drawer/DrawerAdapter.java
@@ -0,0 +1,184 @@
+package ntk.android.ticketing.adapter.drawer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import ntk.android.base.activity.abstraction.AbstractMainActivity;
+import ntk.android.base.activity.common.NotificationsActivity;
+import ntk.android.base.activity.poling.PolingActivity;
+import ntk.android.base.activity.ticketing.FaqActivity;
+import ntk.android.base.activity.ticketing.TicketListActivity;
+import ntk.android.base.adapter.BaseRecyclerAdapter;
+import ntk.android.base.dtomodel.theme.DrawerChildThemeDtoModel;
+import ntk.android.base.room.NotificationStorageService;
+import ntk.android.base.utill.FontManager;
+import ntk.android.ticketing.R;
+import ntk.android.ticketing.activity.AboutUsActivity;
+import ntk.android.ticketing.activity.ArticleListActivity;
+import ntk.android.ticketing.activity.MainActivity;
+import ntk.android.ticketing.activity.NewsListActivity;
+
+
+public class DrawerAdapter extends BaseRecyclerAdapter<DrawerChildThemeDtoModel, DrawerAdapter.ViewHolder> {
+
+
+    private Context context;
+
+    private FlowingDrawer Drawer;
+
+    public DrawerAdapter(Context context, List<DrawerChildThemeDtoModel> children, FlowingDrawer drawer) {
+        super(children);
+        this.context = context;
+        this.Drawer = drawer;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = inflate(viewGroup, R.layout.drawer_theme_1_item);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        ImageLoader.getInstance().displayImage(getItem(position).Icon, holder.Img);
+        if (getItem(position).Type == 1) {
+            int n = new NotificationStorageService().getAllUnread(context).size();
+            if (n != 0) {
+                holder.Lbls.get(1).setText(String.valueOf(n));
+                holder.Lbls.get(1).setVisibility(View.VISIBLE);
+            }
+        }
+        holder.Lbls.get(0).setText(getItem(position).Title);
+
+        holder.Root.setOnClickListener(v -> {
+            switch (getItem(position).Id) {
+                case 1:
+                    ClickInbox();
+                    break;
+                case 2:
+                    ClickNews();
+                    break;
+                case 3:
+                    ClickPooling();
+                    break;
+                case 5:
+                    ClickShare();
+                    break;
+                case 6:
+                    ClickAbout();
+                    break;
+                case 7:
+                    ClickContact();
+                    break;
+                case 8:
+                    ClickFeedBack();
+                    break;
+                case 9:
+                    ClickQuestion();
+                    break;
+                case 10:
+                    ClickArticle();
+                    break;
+            }
+        });
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.ImgRecyclerDrawerChild)
+        ImageView Img;
+
+        @BindViews({R.id.lblRecyclerDrawerChild, R.id.lblBadgeRecyclerDrawerChild})
+        List<TextView> Lbls;
+
+        @BindView(R.id.RootContainerRecyclerDrawerChild)
+        RelativeLayout Root;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+            Lbls.get(0).setTypeface(FontManager.T1_Typeface(context));
+            Lbls.get(1).setTypeface(FontManager.T1_Typeface(context));
+        }
+    }
+
+    private void ClickInbox() {
+        context.startActivity(new Intent(context, NotificationsActivity.class));
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+
+    private void ClickNews() {
+        context.startActivity(new Intent(context, NewsListActivity.class));
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+
+    private void ClickPooling() {
+        context.startActivity(new Intent(context, PolingActivity.class));
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+
+    private void ClickShare() {
+        ((AbstractMainActivity) context).onInviteMethod();
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+
+    private void ClickAbout() {
+        context.startActivity(new Intent(context, AboutUsActivity.class));
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+
+    private void ClickContact() {
+        context.startActivity(new Intent(context, TicketListActivity.class));
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+
+    private void ClickFeedBack() {
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+        ((MainActivity) context).onFeedbackClick();
+    }
+
+    private void ClickQuestion() {
+        context.startActivity(new Intent(context, FaqActivity.class));
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+
+    private void ClickArticle() {
+        context.startActivity(new Intent(context, ArticleListActivity.class));
+        if (Drawer != null) {
+            Drawer.closeMenu(true);
+        }
+    }
+}
